c8bb2b4d17398727cbe6e8b3c5cfd792d5b1540c
Rajat Rasal <yugiohrajat1@gmail.com>
2018-07-19 11:35:12 +0100

    Getting the basic perceptron to predict data and then plotting it in the python notebook

--- perceptron.py
+++ perceptron.py
@@ -5,10 +5,8 @@ from random import randint
 class Perceptron:
     """ Simple Perceptron for classification """
 
-    def __init__(self,
-            rate=0.01 * randint(0, 100),
-            iterations=50,
-            random_gen_seed=1):
+    def __init__(self, rate=0.01 * randint(0, 100), iterations=50, random_gen_seed=1):
+        """ Initialising hyperparameters of the the perceptron """
         # learning rate
         self.rate = rate
         # number of epochs
@@ -17,15 +15,19 @@ class Perceptron:
 
     def fit(self, x, y):
         """ Fits the training data
-        x :: [[n_samples, n_features]] - training vectors
-        y :: [n_samples] - target values
+        x -> [[n_samples, n_features]] (training matrix)
+        y -> [n_samples] (target vector).
+
+        Fitting is the process of training the perceptron and then creating a
+        model against which data can be classified.
         """
+        self.errors = []
+
         # Since the size is an integer, a 1-D array filled with generated
         # values is returned by RandomState
+        # Draws random samples from the Normal Distribution with st_dev of 0.01
         gen = np.random.RandomState(self.random_gen_seed)
-        # Draws random samples from the Normal Distribution with st_dev of 0.01 
         self.w = gen.normal(loc=0.0, scale=0.01, size=1 + x.shape[1])
-        self.errors = []
 
         for _ in range(self.iterations):
             errors = 0
@@ -46,4 +48,4 @@ class Perceptron:
     def predict(self, x):
         """ Threshold function - returns the class label by calculating the
         net input """
-        return np.where(self.net_input(x) >= 0.0, 1, -1)
+        return np.where(self.net_input(x) >= 0, 1, -1)
